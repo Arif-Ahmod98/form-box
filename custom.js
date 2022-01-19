5ec4fda673a8a553740dd162f4761d96ffaeb135
arif ahmod <arifahmod9c@gmial.com>
2022-01-20 00:18:57 +0600

    changed title

--- custom.js
+++ custom.js
@@ -1,31 +1,5 @@
 "use strict";
 
-// const form = document.querySelector("form");
-// const name = form.querySelector("#name");
-// const email = form.querySelector("#email");
-// const password = form.querySelector("#password");
-
-// form.addEventListener("submit", formHandler);
-
-// function formHandler(e) {
-//     e.preventDefault();
-
-// const userInfo = {
-//     name : name.value,
-//     email : email.value,
-//     password : password.value
-// }
-// console.log(userInfo);
-
-// name.value = "";
-// email.value = "";
-// password.value = "";
-
-
-// }
-
-
-
 const form = document.querySelector("form");
 const firstName = form.querySelector("#firstName");
 const lastName = form.querySelector("#lastName");
